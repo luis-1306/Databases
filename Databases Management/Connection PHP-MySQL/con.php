0cd769a9d97b7e4193a291ae1d75dbd5b3a81441
luis-1306 <56192905+luis-1306@users.noreply.github.com>
2019-11-08 13:22:34 +0100

    Update and rename exemple to con.php

---
+++ Databases Management/Connection PHP-MySQL/con.php
@@ -0,0 +1,21 @@
+<?php
+// Conectando y seleccionando la base de datos  
+$dbconn = pg_connect("host=localhost dbname=firstdb user=root password=root*18")
+    or die('No se ha podido conectar con la base de datos' . pg_last_error());
+// Realizando la consulta SQL
+$name = $_POST["inputUser"];
+$pass = $_POST["inputPassword"];
+$query = pg_query($dbconn,"SELECT * FROM access1 WHERE access1.name = '$name' and access1.pass = '$pass'");
+$nr= pg_num_rows($query);
+// Imprimiendo los resultados
+if($nr == 1)
+{
+	echo "Welcome, $name";
+}
+else if ($nr == 0) 
+{
+	echo "Incorrect Credentials"; 
+}
+// Cerrando la conexión
+pg_close($dbconn);
+?>
