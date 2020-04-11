e5305fbf6ebb6f31d33c5692cce8651b65bb3604
luis-1306 <56192905+luis-1306@users.noreply.github.com>
2020-04-12 00:18:07 +0200

    Update dumpbackup.sh

--- Scripts_Bash/dumpbackup.sh
+++ Scripts_Bash/dumpbackup.sh
@@ -3,18 +3,24 @@
 while :
 do
 echo "Selecciona una opcion [1-2]"
-echo "1) Respaldar datos del pendrive"
-echo "2) Recuperar datos"
+echo "1) Respaldar datos del pendrive (copia completa)"
+echo "2) Respaldar datos del pendrive (copia incremental)"
+echo "3) Recuperar datos"
 
 read opcion
 
    case $opcion in
         1)
-        echo "Ejecutando backup..";
+        echo "Ejecutando backup completa..";
         sudo dump 0zf /media/luis/283A9CAC3A1815B4/backup.dump /media/luis/DataTravel;
         exit 1;;
 
         2)
+        echo "Ejecutando backup incremental..";
+        sudo dump 1zf /media/luis/283A9CAC3A1815B4/backup.dump /media/luis/DataTravel;
+        exit 1;;
+
+        3)
         echo "Ejecutando recuperacion de datos..";
         cd /media/luis/283A9CAC3A1815B4/MME_L_Luis_Acosta;
         sudo restore -r -f ../backup.dump;
