8f86590574de5226a38c627bf556d86cc4e2ab8f
Luis Acosta <56192905+luis13byte@users.noreply.github.com>
2021-11-17 16:45:24 +0100

    Upload script for prepare workplace

---
+++ shell-scripts/prepare-workplace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install nmap net-tools yakuake colordiff docker.io go mysql-client filezilla python3 terraform terminator gparted git rkhunter virtualbox vagrant
+mkdir ~/Bucket ~/Docker/ ~/Scripts ~/TestArea
+sudo mkdir -p /opt/git/luis13cst /opt/tools /opt/vagrant/centos7
+
+cat << EOF > /opt/git/.gitignore_global
+# IDEs
+.idea/
+
+.vscode/
+.history/
+EOF
+
+chown ${USER}: /opt/git/ /opt/vagrant/ -R
